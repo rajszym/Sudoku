070ae6e16a8566e003d3ea5a5fcfcbc99e83513f
Szymański Rajmund <szymanski.rajmund@zepak.net>
2020-11-03 07:54:24 +0100

    Update sudoku.cpp

--- c++ directx9/sudoku.cpp
+++ c++ directx9/sudoku.cpp
@@ -29,8 +29,6 @@
 
 ******************************************************************************/
 
-#define WIN32_LEAN_AND_MEAN
-
 #include <windows.h>
 #include <windowsx.h>
 #include "sudoku.hpp"
