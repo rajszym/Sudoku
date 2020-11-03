94b5cd31c3dcde5c6644699dee1192216a467d34
Szymański Rajmund <szymanski.rajmund@zepak.net>
2020-11-03 07:54:17 +0100

    Update sudoku.cpp

--- c++ console/sudoku.cpp
+++ c++ console/sudoku.cpp
@@ -29,8 +29,6 @@
 
 ******************************************************************************/
 
-#define WIN32_LEAN_AND_MEAN
-
 #include <windows.h>
 #include "sudoku.hpp"
 #include "console.hpp"
